6663fd3d8240b125808e414dd26de0173f5d0177
xjin <xjin@suse.com>
2015-07-27 23:24:20 +0800

    modify print function

--- printDict.py
+++ printDict.py
@@ -3,6 +3,7 @@ from pprint import pprint
 
 l1 = ['11sp3','11sp4','12']
 extD = {}
+extD['sp5'] = '23.45%'
 for i in l1:
   intD = extD.setdefault(i,{})
   l2 = ['app','installer','gnome','desktop']
@@ -15,9 +16,11 @@ pprint(extD)
 def printFD(dicD):
   for i in sorted(dicD):
     tmpD = dicD.setdefault(i,dicD[i])
-    for j in sorted(tmpD):
-      print("{} | {} | {}".format(i,j,tmpD[j]))
-
+    if isinstance(tmpD,dict):
+      for j in sorted(tmpD):
+        print("    {} | {} | {}".format(i,j,tmpD[j]))
+    else:
+      print("    {} | {}".format(i,dicD[i]))
 
 printFD(extD)
     
